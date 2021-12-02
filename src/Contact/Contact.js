d93d88ed852ab7dc2318c1e46233be1cc1d264e5
ramshaessa <ressa@luc.edu>
2021-12-01 23:12:33 -0600

    More Changes

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
-import { Component } from 'react';
+
+const formFields = {
+    firstName: "",
+    lastName: "",
+};
+    const ErrorFirstName = "First name should be between five and ten characters.";
+    const ErrorLastName = "Last name should be between five and ten characters.";
+
+    const Contact = () => {
+        const [formObj, setFormObj] = useState({...formFields});
+        const [formObjError, setFormObjError] = useState({...formFields});
 
 
-const Contact = () => {
-    const [firstName, setFirstName] = useState(""); //Make one state for whole form. Inside the initial value should be data. Use one state for error and 
-    const [lastName, setLastName] = useState("");    //Add functionality like drop down, radio button, text area, checkbox etc 
+    }
 
-    const [firstNameErr, setFirstNameErr] = useState({});
-    const [lastNameErr, setLastNameErr] = useState({});
-    const [dropdown, setDropDown] = useState("Java")
-    const [isChecked, setIsChecked] = useState(false)
-    const [radio, setRadio] = useState("Java")
+    const formValidation = () => {
+        const ErrorTempObj = {...formFields};
+    }
 
 
     const onSubmit = (e) => {
@@ -43,73 +49,56 @@ const Contact = () => {
         return isValid;
     }
 
-    return (
-        <form onSubmit={onSubmit}>
-            <label>First Name : </label>
-            <input type="text"
-                value={firstName}
-                onChange={(e) => { setFirstName(e.target.value) }} />
-            <br />
-            {Object.keys(firstNameErr).map((key) => {
-                return <div style={{ color: "red" }}>{firstNameErr[key]}</div>
-            })}
-            <label>Last Name: </label>
-            <input type="text"
-                value={lastName}
-                onChange={(e) => { setLastName(e.target.value) }} />
-            <br />
-            {Object.keys(lastNameErr).map((key) => {
-                return <div style={{ color: "red" }}>{lastNameErr[key]}</div>
-            })}
-            <button type="submit">Submit</button>
+    const onSubmit = () => {
+        formValidation();
+    };
 
-            <h1>Drop Down Value is: {dropdown}</h1>
-            <h1>Checkbox is : {isChecked ? "True" : "False"}</h1>
-            <h1>Radio button is : {radio}</h1>
-            <select value={dropdown} onChange={(e) => { setDropDown(e.target.value) }}>
+    const onChangeFormValue = (name, value) => {
+        setFormObj({
+            ...formObj,
+            [name]: value,
+        });
+    };
+
+    const returnInputField = (label, name) => {
+        return (
+            <>
+            <label>{label}</label>
+            <input
+             type="text"
+             name={name}
+             value={formObj[name]}
+             onChange{(e) => {
+                 onChangeFormValue(e.target.name, e.target.value);
+             }}
+             />
+            <br />
+            {formObjError[name] && <div>{formObjError[name]}</div>}
+            </>
+            };
+        };
+
+        return (
+            <div>
+            {returnInputField("First Name:", "firstName")}
+            {returnInputField("Last Name:", "LastName")}
+            <input type="button" onClick={onSubmit} name="submit" value="submit" />
+            </div>
+
+           
+            );
+
+            return (
+                <select value={dropdown} onChange={(e) => { setDropDown(e.target.value) }}>
                 <option value="Java">Java</option>
                 <option value="Javascript">Javascript</option>
                 <option value="React">React</option>
             </select>
-            <br />
-            <label>Checkbox : </label>
-            <input type="checkbox"
-                checked={isChecked} />
-            onChange={(e) => { setIsChecked(e.target.checked) }}
+            )
 
-            <br />
-            <label>Apple: </label>
-            <input type="radio"
-                checked={radio === "Java"} />
-            value="Java"
-            onChange={(e) => { setRadio(e.target.value) }}
-            <br />
+           
+        };
 
-            <br />
-            <label>Javascript: </label>
-            <input type="radio"
-                checked={radio === "Javascript"} />
-            value="Javascript"
-            onChange={(e) => { setRadio(e.target.value) }}
-            <br />
+        
 
-            <br />
-            <label>React: </label>
-            <input type="radio"
-                checked={radio === "React"} />
-            value="React"
-            onChange={(e) => { setRadio(e.target.value) }}
-            <br />
-
-
-
-        </form>
-
-
-
-
-    )
-
-}
-
-export default Contact
+        export default Contact;
